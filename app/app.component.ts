bcd068d14cb6454526480bd7d49f918d1a7f3e0b
Benni Russell <bennirussell@gmail.com>
2016-08-07 13:05:45 +0100

    adds ngFor repeat on list

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
   title = 'Calorie App';
   developer = 'Benni Russell';
   appColor = 'blue';
+  listType = 'none';
+  drinks = [
+    {id: 1, name: 'tonic water'},
+    {id: 2, name: 'Soda water'},
+    {id: 3, name: 'Ginger Ale'}
+  ];
 
   changeSuitColor() {
     this.appColor = this.appColor === 'blue' ? 'red' : 'blue';
